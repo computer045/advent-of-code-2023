a7c45424b5e6e805b32bcf8524fc1350733a4fb3
Louis-Philippe Fortin <timemaster.lpf@gmail.com>
2023-12-07 00:13:19 -0500

    day 7 init

---
+++ day_07/main.rb
@@ -0,0 +1,25 @@
+def main
+  # Example input
+  input = <<~INPUT
+    32T3K 765
+    T55J5 684
+    KK677 28
+    KTJJT 220
+    QQQJA 483
+  INPUT
+  input = input.split("\n")
+  # input = File.read('./input.txt').split("\n")
+
+  puts part_one(input)
+  puts part_two(input)
+end
+
+def part_one(input)
+  0
+end
+
+def part_two(input)
+  0
+end
+
+main
