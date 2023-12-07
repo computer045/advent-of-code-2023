a8c37ac73f60b9439d2b2a92562a4e6a4d27b756
Louis-Philippe Fortin <timemaster.lpf@gmail.com>
2023-12-06 23:24:10 -0500

    day 6 part 2

--- day_06/main.rb
+++ day_06/main.rb
@@ -4,25 +4,40 @@ def main
     Time:      7  15   30
     Distance:  9  40  200
   INPUT
-  # input = File.read('./input.txt')
-  input = Hash[
+  input = File.read('./input.txt')
+  input1 = Hash[
     [:time, :distance].zip(
       input.split("\n").map{
         |line| line.split(':').last.strip.split(/\s+/).map(&:to_i)
       }
     )
   ]
-  p input
-  puts part_one(input)
-  puts part_two(input)
+  input2 = Hash[
+    [:time, :distance].zip(
+      input.split("\n").map{
+        |line| [line.split(':').last.gsub(/\s/, '').to_i]
+      }
+    )
+  ]
+  puts part_one(input1)
+  puts part_two(input2)
 end
 
 def part_one(input)
-  0
+  distances = []
+  input[:time].each do |time|
+    race_distances = []
+    (0..time).each do |m_sec|
+      race_distances.push(m_sec * (time - m_sec))
+    end
+    distances.push(race_distances)
+  end
+  win_values = distances.each_with_index.map { |race, idx| race.select { |dist| dist > input[:distance][idx] }.size }
+  win_values.inject(:*)
 end
 
 def part_two(input)
-  0
+  part_one(input)
 end
 
 main
